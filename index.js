21be8d1d91fadd8bf1197f6a225a3006d7d34044
Hamzat Balqees <hamzatbalqees0@gmail.com>
2026-09-15 16:13:51 +0100

    added a file

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ const icon2 = document.querySelector("#icon2")
 const kilometer = document.querySelector("#kilometer")
 const icon = document.querySelector("#icon")
 const rain = document.querySelector("#rain")
-const apikey ="ff708158a8de4047ba2143144260709"
-// ...............................................
-function inputScreen() {
-    input.value ="";
-}
+const sun = document.querySelector("#sun")
+
+const apikey = "ff708158a8de4047ba2143144260709"
+
 async function fetchData() {
     try {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${input.value}&aqi=no`);
-        if (response.status === 200){
-            const data = await response.json();
-            console.log(data);
+        const response = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${input.value}&aqi=no`
+        )
+
+        if (response.status === 200) {
+            const data = await response.json()
+
+            console.log(data)
+
             city.innerHTML = data.location.name
             degree.innerHTML = data.current.temp_c
             sun.innerHTML = data.current.condition.text
             degree2.innerHTML = data.current.humidity
             kilometer.innerHTML = data.current.wind_kph
-
-
         }
-
 
     } catch (error) {
         console.log(error)
     }
-
 }
 
 search.addEventListener("click", () => {
